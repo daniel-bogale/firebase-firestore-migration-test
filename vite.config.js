0506b8012ac82046fce94f48d72c1fcbdc89c498
danibog <dannybg090909@gmail.com>
2023-07-08 09:59:26 +0300

    ☹ gitignore is modifiyed

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
-import { defineConfig } from 'vite';
 
-export default defineConfig();
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
